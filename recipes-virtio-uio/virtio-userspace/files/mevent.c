ccc90745dd95bddd40e14745df8429ccc2f7f01d
He Zhe <zhe.he@windriver.com>
2024-03-06 06:25:45 +0000

    Fix license for back end drivers
    Signed-off-by: He Zhe <zhe.he@windriver.com>

--- recipes-virtio-uio/virtio-userspace/files/mevent.c
+++ recipes-virtio-uio/virtio-userspace/files/mevent.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2024, Wind River Systems, Inc.
+ * Copyright (c) 2011 NetApp, Inc.
+ * All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
